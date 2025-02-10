b9ef24eb4bb90eeaa89f7b40bcec564de43bab95
Fagner Tranquilino <fagnertds@gmail.com>
2025-02-10 16:43:49 -0300

    realização dos métodos do CRUD

--- crud/src/main/java/com/fagnertds/crud/services/ClientService.java
+++ crud/src/main/java/com/fagnertds/crud/services/ClientService.java
@@ -1,8 +1,16 @@
 package com.fagnertds.crud.services;
 
+import com.fagnertds.crud.dto.ClientDTO;
+import com.fagnertds.crud.entities.Client;
 import com.fagnertds.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -10,5 +18,47 @@ public class ClientService {
     @Autowired
     ClientRepository repository;
 
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById (Long id) {
+        Client client = repository.findById(id).get();
+        ClientDTO dto = new ClientDTO(client);
+        return dto;
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client client = new Client();
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO update(ClientDTO dto, Long id) {
+        Client client = repository.getReferenceById(id);
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+
+    }
 
 }
